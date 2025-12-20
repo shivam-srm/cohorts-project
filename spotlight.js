b9641d33d18dcea5a92fd14ae13502d56f32b736
Shivam-srm <shivam983840@gmail.com>
2025-12-20 22:20:52 +0530

    assest files moved and styles updated

--- spotlight.js
+++ spotlight.js
@@ -1,15 +1,20 @@
 const para = document.querySelector('p')
 
     const characters ="ASDEFGHGFHDJEKNRBHJKJNMqwertyuioasdghjklxcvbnm"
-    const text = para. innerText
+    const text = para.innerText
+    let iteration=0
     para.addEventListener('mouseenter' , function(){
         setInterval(()=>{
 
         
         const str = text.split('').map((char,index)=>{
+            if(index< iteration){
+                return char
+            }
             return characters.split("") [Math.floor(Math.random() * 53)]
         }).join('')
         para.innerText = str
+        iteration += 0.1
     
         },30)
 })
