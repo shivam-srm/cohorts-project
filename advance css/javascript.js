a8adfa40b0875334564800d5bd477571bc51d1aa
Shivam-srm <shivam983840@gmail.com>
2026-01-20 19:42:21 +0530

    bulk email

--- advance css/javascript.js
+++ advance css/javascript.js
@@ -22,9 +22,48 @@
 // });
 
 
- (async()=>{
-  let raw = await fetch('https://randomuser.me/api/');
-  let data = await raw.json();
-  console.log(data.results[0].name.first);
- })
- ();
+//  (async()=>{
+//   let raw = await fetch('https://randomuser.me/api/');
+//   let data = await raw.json();
+//   console.log(data.results[0].name.first);
+//  })
+//  ();
+
+// sceniaro 1 : Warher Dashboard with error Handling
+
+
+// async function getWeather(city){
+//     let apikey = `e1ffb33de03bdd431636023aa7fdbbac`;
+// let raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
+
+// let realdata = await raw.json();
+// console.log(realdata)
+
+// }
+// getWeather("chennai");
+
+//bulk emalling
+
+const users = [
+    "shivam@xxy.com",
+    "hello@hsdj.com",
+    "why@hello.com"
+];
+
+((email)=>{
+    return new Promise((resolve,reject)=>{
+        let time = Math.floor(Math.random()*5)
+      setTimeout(()=>{
+        let prob = Math.floor(Math.random() * 10);
+        if (prob<=5) {
+            resolve("email send")
+        }else{
+            reject("email not sent")
+        }
+        
+    },time* 1000)
+    })
+})
+("shivam@mail.com")
+.then(msg=> console.log(msg))
+.catch(err=> console.log(err));
